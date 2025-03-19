ffb1f99c14eb4d39584b4f000588107d699aaf00
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2025-03-19 16:34:05 +0300

    initial commit

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { TuiRoot } from '@taiga-ui/core';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, TuiRoot],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
